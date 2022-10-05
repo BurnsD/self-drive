974a9a519b6a20cac838d628d2bb7e442755d9a1
BurnsD <Dustinburnsactech@gmail.com>
2022-10-05 15:20:15 -0500

    refactor changes. road more defined and animation now looks as if car is moving down the moving road

--- js/road.js
+++ js/road.js
@@ -10,6 +10,16 @@ class Road {
     const fakeInfinity = 1000000;
     this.top = -fakeInfinity;
     this.bottom = fakeInfinity;
+
+    // Create Borders of road for collision detection
+    const topLeft = { x: this.left, y: this.top };
+    const bottomLeft = { x: this.left, y: this.bottom };
+    const topRight = { x: this.right, y: this.top };
+    const bottomRight = { x: this.right, y: this.bottom };
+    this.borders = [
+      [topLeft, bottomLeft],
+      [topRight, bottomRight],
+    ];
   }
 
   getLaneCenter(laneindex) {
@@ -17,7 +27,7 @@ class Road {
     return (
       this.left +
       laneWidth / 2 +
-    //   Add minimum value to insure car is within visible lanes
+      //   Add minimum value to insure car is within visible lanes
       Math.min(laneindex, this.laneCount - 1) * laneWidth
     );
   }
@@ -27,17 +37,22 @@ class Road {
     ctx.strokeStyle = "white";
 
     // Calculate and animate lanes
-    for (let i = 0; i <= this.laneCount; i++) {
+    for (let i = 1; i <= this.laneCount - 1; i++) {
       const x = lerp(this.left, this.right, i / this.laneCount);
-      if (i > 0 && i < this.laneCount) {
-        ctx.setLineDash([20, 20]);
-      } else {
-        ctx.setLineDash([]);
-      }
+
+      ctx.setLineDash([20, 20]);
       ctx.beginPath();
       ctx.moveTo(x, this.top);
       ctx.lineTo(x, this.bottom);
       ctx.stroke();
     }
+
+    ctx.setLineDash([]);
+    this.borders.forEach(border=>{
+        ctx.beginPath();
+        ctx.moveTo(border[0].x,border[0].y);
+        ctx.lineTo(border[1].x,border[1].y);
+        ctx.stroke();
+    })
   }
 }
--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-const canvas=document.getElementById("myCanvas");
-canvas.width=200;
+const canvas = document.getElementById("myCanvas");
+canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
-const road=new Road(canvas.width/2,canvas.width*0.9);
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
 // Using getLaneCenter we can position the starting point in a desired lane
-const car=new Car(road.getLaneCenter(2),100,30,50);
+const car = new Car(road.getLaneCenter(2), 100, 30, 50);
 car.draw(ctx);
 
 animate();
 
-function animate(){
-    car.update();
-    canvas.height=window.innerHeight;
-    road.draw(ctx)
-    car.draw(ctx);
-    requestAnimationFrame(animate);
+function animate() {
+  car.update();
+  canvas.height = window.innerHeight;
+
+  ctx.save();
+  ctx.translate(0, -car.y+canvas.height*0.5);
+
+  road.draw(ctx);
+  car.draw(ctx);
+  requestAnimationFrame(animate);
 }
